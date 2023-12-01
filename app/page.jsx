316b666880e5b61843e78158a499276c8b6c395a
QTN-dev <quangtn12@fpt.com.vn>
2023-12-01 11:36:40 +0700

    Update datepicker

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,15 @@
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import Image from 'next/image'
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Image from "next/image";
 
 export default function Home() {
-  return <main className='max-w-[1440px] mx-auto bg-white overflow-hidden'> 
-    <Header />
-    <Hero />
-    <Footer />
-    <div className='h-[4000px]'></div>
-  </main>
+  return (
+    <main className="max-w-[1440px] mx-auto bg-white overflow-hidden">
+      <Header />
+      <Hero />
+      <Footer />
+      <div className="h-[4000px]"></div>
+    </main>
+  );
 }
--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -1,24 +1,24 @@
-'use client';
-import { Input } from './ui/input';
-import { Label } from './ui/label';
-import { Button } from './ui/button';
-import { Checkbox } from './ui/checkbox';
-import { MapPin } from 'lucide-react';
+"use client";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Button } from "./ui/button";
+import { Checkbox } from "./ui/checkbox";
+import { MapPin } from "lucide-react";
 
-import { motion } from 'framer-motion';
-import { fadeIn } from '../variant';
-
+import { motion } from "framer-motion";
+import { fadeIn } from "../variant";
+import DatePicker from "./DatePicker";
 
 const SearchBox = () => {
   return (
-    <div className='bg-pink'>
+    <div className="bg-pink">
       {/* input, calendar, date picker */}
       <div>
         <Label htmlFor="destination">Where are you going?</Label>
         {/* input & icon */}
-        <div className='relative flex items-center mb-[20px]'>
-          <Input id='destination' placeholder= 'try "Singapore"' />
-          <MapPin size={24} className='absolute right-6 text-black' />
+        <div className="relative flex items-center mb-[20px]">
+          <Input id="destination" placeholder='try "Singapore"' />
+          <MapPin size={24} className="absolute right-6 text-black" />
         </div>
         {/* date picker */}
         <div>
@@ -30,7 +30,7 @@ const SearchBox = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBox
+export default SearchBox;
