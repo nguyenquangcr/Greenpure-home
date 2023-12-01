46de9561eb96005f93c6ee4b25948004a19cd2ce
QTN-dev <quangtn12@fpt.com.vn>
2023-12-01 17:26:51 +0700

    End UI hero component

--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -8,12 +8,19 @@ import { MapPin } from "lucide-react";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variant";
 import DatePicker from "./DatePicker";
+import GuestSelect from "./GuestSelect";
 
 const SearchBox = () => {
   return (
-    <div className="bg-pink">
+    <motion.div
+      variants={fadeIn("up", 0.2)}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: false, amount: 0 }}
+      className="w-full max-w-[488px] bg-white max-h-max flex flex-col border border-outline rounded-[20px] p-10"
+    >
       {/* input, calendar, date picker */}
-      <div>
+      <div className="mb-[20px]">
         <Label htmlFor="destination">Where are you going?</Label>
         {/* input & icon */}
         <div className="relative flex items-center mb-[20px]">
@@ -21,15 +28,40 @@ const SearchBox = () => {
           <MapPin size={24} className="absolute right-6 text-black" />
         </div>
         {/* date picker */}
-        <div>
+        <div className="flex flex-col xl:flex-row gap-x-[30px] gap-y-5 xl:gap-y-0">
           {/* check-in */}
-          <div>
+          <div className="flex flex-col flex-1">
             <Label>Check in</Label>
+            <DatePicker />
+          </div>
+          {/* check-out */}
+          <div className="flex flex-col flex-1">
+            <Label>Check out</Label>
             <DatePicker />
           </div>
         </div>
       </div>
-    </div>
+
+      {/* select, checkbox, btn */}
+      <div className="flex flex-col">
+        {/* select */}
+        <div className="flex flex-col flex-1 mb-[24px]">
+          <Label>Guest</Label>
+          <GuestSelect />
+        </div>
+        {/* checkbox */}
+        <div className="flex items-center gap-x-3 mb-[24px]">
+          <Checkbox id="terms" />
+          <Label htmlFor="terms" className="font-semibold mb-0">
+            Pay when checking in?
+          </Label>
+        </div>
+        {/* btn */}
+        <Button size="lg" variant="accent">
+          Search Hotel
+        </Button>
+      </div>
+    </motion.div>
   );
 };
 
